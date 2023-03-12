c50b610990ccc877d5c413f144b619e4cb97e409
alyxiskitty <alice.weeks@northumbria.ac.uk>
2023-03-12 19:07:27 +0000

    blog page created

---
+++ src/components/BlogMain.js
@@ -0,0 +1,16 @@
+/**
+ * Main blog page displays the featured blog posts and all posts with filters 
+ * 
+ */
+
+function BlogPage() {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+
+export { BlogPage };
