e518df55dd65eacff5de8eaddb40bad3e295fbba
alyxiskitty <alice.weeks@northumbria.ac.uk>
2023-03-18 16:32:49 +0000

    blog edits

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { doc, getDoc } from 'firebase/firestore';
-import { db } from '../Firebase';
+import { ref, getDownloadURL } from "firebase/storage";
+import { db, storage } from '../Firebase';
 import { useParams } from 'react-router-dom';
 import { NoImageSlider } from "../components/index.js";
 
@@ -12,12 +13,16 @@ import { NoImageSlider } from "../components/index.js";
 function BlogPost() {
     const { id } = useParams();
     const [post, setPost] = useState(null);
+    const [postImageURL, setPostImageURL] = useState('');
 
     useEffect(() => {
         const fetchPost = async () => {
             const postDoc = await getDoc(doc(db, "blogPosts", id));
             if (postDoc.exists()) {
-                setPost({ id: postDoc.id, ...postDoc.data() });
+                const postImageRef = ref(storage, `blog/${id}.jpg`);
+                const imageUrl = await getDownloadURL(postImageRef);
+                setPostImageURL(imageUrl);
+                setPost({ id: postDoc.id, ...postDoc.data(), post_image_url: imageUrl });
             }
         }
         fetchPost();
@@ -41,28 +46,27 @@ function BlogPost() {
     }
 
     return (
-        <div className="blog__wrapper d-flex flex-wrap">
-                <div className="blog__item mb-60 flex-grow-1" key={post.id}>
-                    <div className="blog__thumb fix">
-                        
-                        
-                            <img src={post.postImage} alt={post.postTitle} />
-                        
-                    </div>
-                    <div className="blog__content">
-                        
-                            <h4>{post.postTitle}</h4>
-                        
-                        <div className="blog__meta">
-                            <span>By <a href="#">{post.author}</a></span>
-                            <span> {formatDate(post.datePosted)}</span>
+        <section class="blog__area pt-55">
+            <div class="container d-flex flex-wrap">
+                
+                    <div>
+                        <div class="postbox__title mb-40" key={post.id}>
+                            <h2 className="black-color">{post.postTitle}</h2>
+                            <div className="blog__meta">
+                                <span>By <a href="#">{post.author}</a></span>
+                                <span> {formatDate(post.datePosted)}</span>
+                            </div>
                         </div>
-                        <p>{post.postBody}</p>
-                       
+                        <div class="postbox__details-img mb-60">
+                            <img src={post.post_image_url} alt={post.postTitle} />
+                        </div>
                     </div>
-                </div>
-            
-        </div>
+                    <div>
+                        <p className="black-color mt-30 ml-30">{post.postBody}</p>  
+                    </div>
+                
+            </div>
+        </section>
     );
 }
 
