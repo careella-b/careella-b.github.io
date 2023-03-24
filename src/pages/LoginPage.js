1477542bcea751747540a5038777a83b2deb370a
chloth <chloethompson432@gmail.com>
2023-03-24 18:34:38 +0000

    login page edit

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+
 /**
  * Login page displays the log-in form 
  * 
@@ -6,17 +9,58 @@ import { Link } from "react-router-dom";
 
 function LoginPage() {
 
+        /*const auth = getAuth();
+            signInWithEmailAndPassword(auth, email.value, pass.value)
+            .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            // ...
+        })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+        });*/
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const auth = getAuth();
+
+    const signIn = (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log("signed in");
+        })
+        .catch((error => {
+            console.log(error);
+        }))
+    }
+    
     return (
         <section className="login-area pt-100 pb-100">
             <div className="container d-flex justify-content-center">
                 <div className="col-lg-7 ">
                     <div className="basic-login">
                         <h3 className="text-center black-color mb-30">Log In</h3>
-                        <form action="#">
-                            <label htmlFor="name">Email Address <span>*</span></label>
-                            <input id="name" type="text" placeholder="Enter email address..." />
+                        <form onSubmit={signIn}>
+                            <label htmlFor="email">Email Address <span>*</span></label>
+                            <input 
+                                id="email" 
+                                type="text" 
+                                placeholder="Enter email address..." 
+                                value={email}
+                                onChange={(e)=> setEmail(e.target.value)}
+                            />
                             <label htmlFor="pass">Password <span>*</span></label>
-                            <input id="pass" type="password" placeholder="Enter password..." />
+                            <input 
+                                id="pass" 
+                                type="password" 
+                                placeholder="Enter password..." 
+                                value={password}
+                                onChange={(e)=> setPassword(e.target.value)}
+                            />
                             <div className="login-action mb-20 fix">
                                 <span className="log-rem f-left">
                                     <input id="remember" type="checkbox" />
@@ -27,7 +71,7 @@ function LoginPage() {
                                 </span>
                             </div>
                             <div className="d-flex justify-content-center">
-                                <button className="os-btn bw-50">Login</button>
+                                <button className="os-btn bw-50" type="submit">Login</button>
                             </div>
                             <div className="or-divide"></div>
                             <div className="align-items-center text-center">
